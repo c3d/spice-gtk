475d8b8ee3b1421d2df8ee5509f3f808809968e2
Christophe de Dinechin <dinechin@redhat.com>
2017-05-31 12:16:53 +0200

    Trace for channel

--- src/spice-channel-priv.h
+++ src/spice-channel-priv.h
@@ -40,8 +40,9 @@ G_BEGIN_DECLS
 
 #define MAX_SPICE_DATA_HEADER_SIZE sizeof(SpiceDataHeader)
 
-#define CHANNEL_DEBUG(channel, fmt, ...) \
-    SPICE_DEBUG("%s: " fmt, SPICE_CHANNEL(channel)->priv->name, ## __VA_ARGS__)
+#define CHANNEL_DEBUG(chan, fmt, ...)                                   \
+    spice_trace(channel, "%s: " fmt,                                    \
+                SPICE_CHANNEL(chan)->priv->name, ## __VA_ARGS__)
 
 struct _SpiceMsgOut {
     int                   refcount;
